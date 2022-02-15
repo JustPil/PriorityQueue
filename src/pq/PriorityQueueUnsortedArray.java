4a6e3c120439c773f04e78a5a8805d9ec6dc931a
JustPil <88640664+JustPil@users.noreply.github.com>
2022-02-15 09:28:28 -0800

    Minor style updates

--- src/pq/PriorityQueueUnsortedArray.java
+++ src/pq/PriorityQueueUnsortedArray.java
@@ -2,8 +2,7 @@ package pq;
 
 import java.util.Comparator;
 
-public class PriorityQueueUnsortedArray<T> implements PriorityQueueInterface<T>
-{
+public class PriorityQueueUnsortedArray<T> implements PriorityQueueInterface<T> {
     private T[] array;
     private int last = 0;
     private Comparator<T> comp;
@@ -11,13 +10,11 @@ public class PriorityQueueUnsortedArray<T> implements PriorityQueueInterface<T>
     /**
      * Constructor creates a new unsorted Array Priority Queue with default array capacity of 100.
      */
-    public PriorityQueueUnsortedArray()
-    {
+    public PriorityQueueUnsortedArray() {
         array = (T[]) new Object[100];
-        comp = new Comparator<T>()
-        {
-            public int compare(T o1, T o2)
-            {
+        comp = new Comparator<T>() {
+            @Override
+            public int compare(T o1, T o2) {
                 return ((Comparable)o1).compareTo(o2);
             }
         };
@@ -27,13 +24,11 @@ public class PriorityQueueUnsortedArray<T> implements PriorityQueueInterface<T>
      * Constructor creates a new unsorted Array Priority Queue with specified array capacity.
      * @param bound The capacity of the array.
      */
-    public PriorityQueueUnsortedArray(int bound)
-    {
+    public PriorityQueueUnsortedArray(int bound) {
         array = (T[]) new Object[bound];
-        comp = new Comparator<T>()
-        {
-            public int compare(T o1, T o2)
-            {
+        comp = new Comparator<T>() {
+            @Override
+            public int compare(T o1, T o2) {
                 return ((Comparable)o1).compareTo(o2);
             }
         };
@@ -43,10 +38,8 @@ public class PriorityQueueUnsortedArray<T> implements PriorityQueueInterface<T>
      * Adds a new element to the Priority Queue.
      * @param item The element to be added.
      */
-    public void enqueue(T item)
-    {
-        if(!isFull())
-        {
+    public void enqueue(T item) {
+        if(!isFull()) {
             array[last++] = item;
         }
     }
@@ -55,30 +48,24 @@ public class PriorityQueueUnsortedArray<T> implements PriorityQueueInterface<T>
      * dequeue Removes the highest priority element from the Priority Queue.
      * @return The highest priority element.
      */
-    public T dequeue()
-    {
-        if(isEmpty())
-        {
+    public T dequeue() {
+        if(isEmpty()) {
             return null;
         }
-        if(last == 1)
-        {
+        if(last == 1) {
             last--;
             T ret = array[last];
             array[last] = null;
             return ret;
         }
         T highest = findHighest();
-        if(comp.compare(array[last - 1], highest) == 0)
-        {
+        if(comp.compare(array[last - 1], highest) == 0) {
             array[last - 1] = null;
             last--;
             return highest;
         }
-        for(int i = 0; i < last; i++)
-        {
-            if(comp.compare(array[i], highest) == 0)
-            {
+        for(int i = 0; i < last; i++) {
+            if(comp.compare(array[i], highest) == 0) {
                 array[i] = array[last - 1];
                 array[last--] = null;
             }
@@ -90,13 +77,10 @@ public class PriorityQueueUnsortedArray<T> implements PriorityQueueInterface<T>
      * findHighest Searches the array for the highest priority element.
      * @return The value of the highest priority element.
      */
-    private T findHighest()
-    {
+    private T findHighest() {
         T highest = array[0];
-        for(int i = 1; i < last; i++)
-        {
-            if(comp.compare(array[i], highest) >= 0)
-            {
+        for(int i = 1; i < last; i++) {
+            if(comp.compare(array[i], highest) >= 0) {
                 highest = array[i];
             }
         }
@@ -107,8 +91,7 @@ public class PriorityQueueUnsortedArray<T> implements PriorityQueueInterface<T>
      * isEmpty Reports if the Priority Queue is empty.
      * @return True if the Priority Queue is empty, false otherwise.
      */
-    public boolean isEmpty()
-    {
+    public boolean isEmpty() {
         return last == 0;
     }
 
@@ -116,8 +99,7 @@ public class PriorityQueueUnsortedArray<T> implements PriorityQueueInterface<T>
      * isFull Reports if the Priority Queue is full.
      * @return True if the Priority Queue is full, false otherwise.
      */
-    public boolean isFull()
-    {
+    public boolean isFull() {
         return last == array.length;
     }
 
@@ -125,8 +107,7 @@ public class PriorityQueueUnsortedArray<T> implements PriorityQueueInterface<T>
      * size Reports the total number of elements contained in the Priority Queue.
      * @return The count of all elements contained in the Priority Queue.
      */
-    public int size()
-    {
+    public int size() {
         return last;
     }
 }
