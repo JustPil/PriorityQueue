9bae14844645e02b33e9fee059599ee91ce3e9de
JustPil <88640664+JustPil@users.noreply.github.com>
2022-02-15 09:28:02 -0800

    Minor style updates

--- src/pq/PriorityQueueSortedLL.java
+++ src/pq/PriorityQueueSortedLL.java
@@ -2,8 +2,7 @@ package pq;
 
 import java.util.Comparator;
 
-public class PriorityQueueSortedLL<T> implements PriorityQueueInterface<T>
-{
+public class PriorityQueueSortedLL<T> implements PriorityQueueInterface<T> {
     private Node<T> front;
     private int total = 0;
     private Comparator<T> comp;
@@ -11,13 +10,11 @@ public class PriorityQueueSortedLL<T> implements PriorityQueueInterface<T>
     /**
      * Constructor creates a new sorted Linked List Priority Queue.
      */
-    public PriorityQueueSortedLL()
-    {
+    public PriorityQueueSortedLL() {
         front = null;
-        comp = new Comparator<T>()
-        {
-            public int compare(T element1, T element2)
-            {
+        comp = new Comparator<T>() {
+            @Override
+            public int compare(T element1, T element2) {
                 return ((Comparable)element1).compareTo(element2);
             }
         };
@@ -27,38 +24,26 @@ public class PriorityQueueSortedLL<T> implements PriorityQueueInterface<T>
      * Adds a new element to the Priority Queue in sorted order.
      * @param item The element to be added.
      */
-    public void enqueue(T item)
-    {
+    public void enqueue(T item) {
         Node<T> node = new Node<>(item);
         Node<T> parser = front;
-        if(isEmpty())
-        {
+        if(isEmpty()) {
             front = node;
-        }
-        else
-        {
-            while(parser != null)
-            {
-                if(parser == front && comp.compare(front.getData(), item) <= 0)
-                {
+        } else {
+            while(parser != null) {
+                if(parser == front && comp.compare(front.getData(), item) <= 0) {
                     node.setNext(front);
                     front = node;
                     break;
-                }
-                else if(parser.getNext() != null && comp.compare(parser.getNext().getData(), item) <= 0)
-                {
+                } else if(parser.getNext() != null && comp.compare(parser.getNext().getData(), item) <= 0) {
                     Node<T> temp = parser.getNext();
                     parser.setNext(node);
                     node.setNext(temp);
                     break;
-                }
-                else if(parser.getNext() == null)
-                {
+                } else if(parser.getNext() == null) {
                     parser.setNext(node);
                     break;
-                }
-                else
-                {
+                } else {
                     parser = parser.getNext();
                 }
             }
@@ -70,21 +55,16 @@ public class PriorityQueueSortedLL<T> implements PriorityQueueInterface<T>
      * dequeue Removes the highest priority element from the Priority Queue.
      * @return The highest priority element.
      */
-    public T dequeue()
-    {
-        if(isEmpty())
-        {
+    public T dequeue() {
+        if(isEmpty()) {
             return null;
         }
         T highest = front.getData();
-        if(total == 1)
-        {
+        if(total == 1) {
             front = null;
             total--;
             return highest;
-        }
-        else
-        {
+        } else {
             front = front.getNext();
         }
         total--;
@@ -95,8 +75,7 @@ public class PriorityQueueSortedLL<T> implements PriorityQueueInterface<T>
      * isEmpty Reports if the Priority Queue is empty.
      * @return True if the Priority Queue is empty, false otherwise.
      */
-    public boolean isEmpty()
-    {
+    public boolean isEmpty() {
         return total == 0;
     }
 
@@ -104,8 +83,7 @@ public class PriorityQueueSortedLL<T> implements PriorityQueueInterface<T>
      * isFull Reports if the Priority Queue is full.
      * @return True if the Priority Queue is full, false otherwise.
      */
-    public boolean isFull()
-    {
+    public boolean isFull() {
         return false;
     }
 
@@ -113,8 +91,7 @@ public class PriorityQueueSortedLL<T> implements PriorityQueueInterface<T>
      * size Reports the total number of elements contained in the Priority Queue.
      * @return The count of all elements contained in the Priority Queue.
      */
-    public int size()
-    {
+    public int size() {
         return total;
     }
 }
