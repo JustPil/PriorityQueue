2fa6ee0bce11145de3139b017da57c54d87403d4
JustPil <88640664+JustPil@users.noreply.github.com>
2022-02-15 09:25:27 -0800

    Minor style updates

--- src/pq/BinarySearchTreeInterface.java
+++ src/pq/BinarySearchTreeInterface.java
@@ -1,25 +1,14 @@
 package pq;
 
-public interface BinarySearchTreeInterface<T>
-{
-    StringBuilder inOrder();
-    StringBuilder preOrder();
-    StringBuilder postOrder();
-    StringBuilder levelOrder();
+public interface BinarySearchTreeInterface<T> {
     T minNode();
     T maxNode();
     boolean isFull();
     boolean isEmpty();
-    int leafCounter();
-    int onlyChild();
     int height();
     int size();
     boolean search(T data);
     boolean insert(T data);
     boolean remove(T data);
-    BSTNode<T> replacement(BSTNode<T> n);
     BSTNode<T> balance(int start, int end);
-    void setRoot(BSTNode<T> n);
-    double fullnessRatio();
-    int optimalHeight();
 }
