9cdcd006c0dd781e3c6b410d54c8d9ab3a120d6b
JustPil <88640664+JustPil@users.noreply.github.com>
2023-03-13 19:55:42 -0700

    Delete src directory

--- src/main/java/priorityqueues/PriorityQeueuInterface.java
+++
@@ -1,9 +0,0 @@
-package priorityqueues;
-
-public interface PriorityQeueuInterface<T> {
-    void enqueue(T item);
-    T dequeue();
-    boolean isEmpty();
-    boolean isFull();
-    int size();
-}
